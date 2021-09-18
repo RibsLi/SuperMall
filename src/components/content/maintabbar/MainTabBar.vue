3d6984485643878a35df166748a5a5e4a4f3b48f
RibsLi <2234692411@qq.com>
2021-09-18 23:10:13 +0800

    创建TabBar等组件并解决cli3/4部分版本问题

---
+++ src/components/content/maintabbar/MainTabBar.vue
@@ -0,0 +1,68 @@
+<template>
+  <tab-bar>
+    <tab-bar-item path="/home">
+      <template v-slot:item-icon>
+        <img src="~assets/img/tabbar/icon01.png" alt="">
+      </template>
+      <template v-slot:item-icon-active>
+        <img src="~assets/img/tabbar/icon01_active.png" alt="">
+      </template>
+      <template v-slot:item-text>
+        <div>首页</div>
+      </template>
+    </tab-bar-item>
+    <tab-bar-item path="/category">
+      <template v-slot:item-icon>
+        <img src="~assets/img/tabbar/icon02.png" alt="">
+      </template>
+      <template v-slot:item-icon-active>
+        <img src="~assets/img/tabbar/icon02_active.png" alt="">
+      </template>
+      <template v-slot:item-text>
+        <div>分类</div>
+      </template>
+    </tab-bar-item>
+    <tab-bar-item path="/cart">
+      <template v-slot:item-icon>
+        <img src="~assets/img/tabbar/icon03.png" alt="">
+      </template>
+      <template v-slot:item-icon-active>
+        <img src="~assets/img/tabbar/icon03_active.png" alt="">
+      </template>
+      <template v-slot:item-text>
+        <div>购物车</div>
+      </template>
+    </tab-bar-item>
+    <tab-bar-item path="/profile">
+      <template v-slot:item-icon>
+        <img src="~assets/img/tabbar/icon04.png" alt="">
+      </template>
+      <template v-slot:item-icon-active>
+        <img src="~assets/img/tabbar/icon04_active.png" alt="">
+      </template>
+      <template v-slot:item-text>
+        <div>个人</div>
+      </template>
+    </tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+  import TabBar from 'components/common/tabbar/TabBar.vue'
+  import TabBarItem from 'components/common/tabbar/TabBarItem.vue'
+
+  export default {
+    name: "MainTabBar",
+    components: {
+      TabBar,
+      TabBarItem
+    }
+  }
+</script>
+
+<style scoped>
+  .tab-bar-item img {
+    width: 24px;
+    height: 24px;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+// import Vue from 'vue'
+// import App from './App'
+// import router from './router'
+
+// // Vue.config.productionTip = false
+
+// /* eslint-disable no-new */
+// new Vue({
+//   render: h => h(App),
+//   router,
+// }).$mount("#app")
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router/index.js'
+// import store from './store'
+
+createApp(App).use(router).mount('#app')
