43bf3c8fcdaef018e160903ef1aefb53ebf13fe2
RibsLi <2234692411@qq.com>
2021-09-27 22:46:53 +0800

    完成首页TabControl的切换展示

--- src/components/content/goods/GoodsList.vue
+++ src/components/content/goods/GoodsList.vue
@@ -5,8 +5,8 @@
       <div class="goods-info">
         <p>{{item.title}}</p>
         <span class="price">{{item.price}}</span>
-        <i class="cfav"></i>
-        <span>{{item.cfav}}</span>
+        <!-- <i class="cfav"></i> -->
+        <span class="cfav">{{item.cfav}}</span>
       </div>
     </div>
   </div>
@@ -34,16 +34,19 @@ export default {
 }
 .goods-item {
   width: 48%;
-  margin-top: 2px;
+  padding-bottom: 40px;
+  position: relative;
 }
 .goods-item img {
   width: 100%;
   border-radius: 5px;
 }
 .goods-info {
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 5px;
   font-size: 12px;
-  overflow: hidden;
-  white-space: nowrap;
   text-align: center;
 }
 .goods-info p {
@@ -54,16 +57,28 @@ export default {
 }
 .goods-info .price {
   color: var(--color-high-text);
-  /* margin-right: 10px; */
+  margin-right: 20px;
 }
 
-.goods-info .cfav {
+/* .goods-info .cfav {
   display: inline-block;
   margin: 0 5px;
   width: 12px;
   height: 12px;
   background: url("~assets/img/common/collect.png") no-repeat;
   background-size: cover;
-	/* vertical-align: bottom; */
+	vertical-align: bottom;
+} */
+.goods-info .cfav {
+  position: relative;
+}
+.goods-info .cfav::before {
+  content: '';
+  position: absolute;
+  left: -15px;
+  top: -1px;
+  width: 14px;
+  height: 14px;
+  background: url("~assets/img/common/collect.svg") 0 0/14px 14px;
 }
 </style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -6,20 +6,9 @@
     <home-swiper :banners="banners"></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
     <feature-view></feature-view>
-    <tab-control class="tab-control" :titles="['流行', '新款', '精选']"></tab-control>
-    <goods-list :goods="goods['pop'].list"></goods-list>
-    <ul>
-      <li>liebiao1</li>
-      <li>liebiao2</li>
-      <li>liebiao3</li>
-      <li>liebiao4</li>
-      <li>liebiao5</li>
-      <li>liebiao6</li>
-      <li>liebiao7</li>
-      <li>liebiao8</li>
-      <li>liebiao9</li>
-      <li>liebiao10</li>
-    </ul>
+    <tab-control class="tab-control" :titles="['流行', '新款', '精选']" @tabClick="tabClick"></tab-control>
+    <goods-list :goods="showGoods"></goods-list>
+    
   </div>
 </template>
 
@@ -59,7 +48,8 @@
           'pop': {page: 0, list: []},
           'new': {page: 0, list: []},
           'sell': {page: 0, list: []},
-        }
+        },
+        currentType: 'pop'
       }
     },
     created() {//created里面的方法都封装到了methods里面
@@ -84,13 +74,34 @@
         // [type]是因为他是变了所以不能写成.type
         const page = this.goods[type].page +1
         getHomeGoods(type, page).then(res => {
-          console.log(res);
+          // console.log(res);
           // ...表示可变参数也是一种解构方式，把新拿到的数组一个一个加到原有的数组中，也可以使用for循环一个个加到原数组中，此处不能用=这样会覆盖原有的数组
           this.goods[type].list.push(...res.data.data.list)
           this.goods[type].page += 1
         })
+      },
+      // TabControl点击切换商品类型
+      tabClick(index) {
+        console.log(index);
+        switch (index) {
+          case 0: 
+            this.currentType = 'pop'
+            break
+          case 1:
+            this.currentType = 'new'
+            break
+          case 2:
+            this.currentType = 'sell'
+            break
+        }
       }
       
+    },
+    computed: {
+      //显示商品
+      showGoods() {
+        return this.goods[this.currentType].list
+      }
     }
   }
 </script>
@@ -111,5 +122,6 @@
   .tab-control {
     position: sticky;
     top: 44px;
+    z-index: 9;
   }
 </style>
