ed9c5a83ba3bec71905428f48c0682c22b219f57
RibsLi <2234692411@qq.com>
2021-10-26 12:28:55 +0800

    完成商品分类界面

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -6,6 +6,8 @@
     <category-list :categories="categories" @categoryClick="categoryClick" />
     <scroll class="content1" :pull-up-load="true">
       <category-list-item :subcategoryList="subcategoryList" />
+      <tab-control :titles="titles" @tabClick="tabClick" ref="tabControl" />
+      <goods-list :goods="categoryDetailList" />
     </scroll>
   </div>
 </template>
@@ -16,6 +18,8 @@ import Scroll from "components/common/scroll/Scroll";
 import BackTop from "components/content/backTop/BackTop";
 import CategoryList from "./childComps/CategoryList";
 import CategoryListItem from "./childComps/CategoryListItem";
+import TabControl from "components/content/tabControl/TabControl";
+import GoodsList from "components/content/goods/GoodsList";
 
 import {
   getCategory,
@@ -30,6 +34,8 @@ export default {
       categories: [],
       currentIndex: 0,
       subcategoryList: [],
+      titles: ["流行", "新款", "精选"],
+      categoryDetailList: [],
     };
   },
   components: {
@@ -37,6 +43,8 @@ export default {
     Scroll,
     CategoryList,
     CategoryListItem,
+    TabControl,
+    GoodsList,
   },
   created() {
     this.getCategory();
@@ -46,26 +54,42 @@ export default {
     // 请求分类列表数据
     getCategory() {
       getCategory().then((res) => {
-        console.log(res);
+        // console.log(res);
         this.categories = res.data.data.category.list;
         this.$nextTick(() => {
           this.getSubcategory(this.categories[0].maitKey);
-          // this.getCategoryDetail(this.categoryList[0].miniWallkey, "pop");
+          this.getCategoryDetail(this.categories[0].miniWallkey, "pop");
         });
       });
     },
     // 请求分类详情数据
     categoryClick({ maitKey, index }) {
       this.currentIndex = index;
-      this.getSubcategory(maitKey)
+      this.getSubcategory(maitKey);
+      // 每次切换分类初始化tabControl的下标
+      this.$refs.tabControl.currentIndex = 0;
+      // 请求对应的推荐列表
+      this.getCategoryDetail(this.categories[this.currentIndex].miniWallkey, "pop");
     },
-    // 请求右侧分类菜单
+    // 请求右侧分类菜单数据
     getSubcategory(key) {
       getSubcategory(key).then((res) => {
-        console.log(res);
+        // console.log(res);
         this.subcategoryList = res.data.data.list;
       });
     },
+    // 请求分类商品数据
+    getCategoryDetail(key, type) {
+      getCategoryDetail(key, type).then((res) => {
+        // console.log(res);
+        this.categoryDetailList = res.data;
+      });
+    },
+    tabClick(index) {
+      const typeList = ["pop", "new", "sell"];
+      // 切换类型数据
+      this.getCategoryDetail(this.categories[this.currentIndex].miniWallkey, typeList[index]);
+    }
   },
 };
 </script>
